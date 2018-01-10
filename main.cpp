181e3e456ea81464a35d2c14bff5c7e5b78f0ed2
AdrienFraysse <fraysse.ad@gmail.com>
2018-01-10 20:21:04 +0100

    Commentaire vecteurs

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ void ajouter_lettre(ifstream &is, charVector & vector)
 {
 	while(is.good)
 	{
+		//Met tous les caractères dans le vecteur.
 		char c;
 		is.get(c);
 		vector.pushback(c);
@@ -21,6 +22,7 @@ void ajouter_lettre(ifstream &is, charVector & vector)
 void compter_lettrer(Mymap &map, charVector &vector)
 {
 		size_t taille = vector.size();
+		//Met tous les caractères du vecteur dans la map et compte le nombre d'occurence.
 		for(size_t i=0; i<taille ; ++i)
 		{
 			map[vector[i]]+=1;
@@ -35,12 +37,15 @@ int main()
 	ifstream is2("swann10.txt");
 	if(is1 & is2)
 	{
+		//Stock tous les caractères des différents fichiers dans un vecteur
 		ajouter_lettre(is1, cVector);
 		ajouter_lettre(is2, cVector);
-		
+		//Ajoute tous les caractères dans une map (trié par ordre croissant) pour obtenir le caractère et son nombre d'occurence
 		compter_lettre(Map, cVector);
+		
 	}
 	else
+		//Si impossible de lire un des fichiers d'entrée
 		cout << "Erreur de lecture " << endl;
 	return 0;
 }
